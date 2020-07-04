71eb650932220773eabd8dd6a800a589668d73cf
Ricardo Silva <ricardoslv688@gmail.com>
2020-07-04 12:08:10 +0100

    refactoring tests

--- server/src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ server/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -2,13 +2,18 @@ import AppError from '@shared/errors/App.error';
 import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
 import CreateAppointmentService from './CreateAppointmentService';
 
+let fakeAppointmentsRepository: FakeAppointmentsRepository;
+let createAppointments: CreateAppointmentService;
+
 describe('CreateAppointment', () => {
-  it('should be able to create a new appointment', async () => {
-    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
-    const createAppointments = new CreateAppointmentService(
+  beforeEach(() => {
+    fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    createAppointments = new CreateAppointmentService(
       fakeAppointmentsRepository
     );
+  });
 
+  it('should be able to create a new appointment', async () => {
     const appointment = await createAppointments.execute({
       date: new Date(),
       provider_id: '123132',
@@ -19,11 +24,6 @@ describe('CreateAppointment', () => {
   });
 
   it('shoud not be able to create two appointments on the same time', async () => {
-    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
-    const createAppointments = new CreateAppointmentService(
-      fakeAppointmentsRepository
-    );
-
     const appointmentDate = new Date(2020, 4, 10, 11);
 
     await createAppointments.execute({
